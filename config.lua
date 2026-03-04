f3790ee741dc7e43d5078589dd78e6ede78cde7e
Dougg Junnior <douggjunn@outlook.com>
2026-03-04 04:45:47 -0300

    Rev (1.0.0.0)

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = Config or {}
+
+Config.itemListSwimming = {{
+    ['normalItem'] = 'phone',
+    ['damageItem'] = 'phone-damaged'
+}, {
+    ['normalItem'] = 'rolling_paper',
+    ['damageItem'] = nil
+}}
+
+Config.itemListAccident = {{
+    ['normalItem'] = 'phone',
+    ['damageItem'] = 'phone-broken'
+}}
